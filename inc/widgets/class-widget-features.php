1114ed80054dd095fd0cfe53e6ac344789f4198a
Fernando <isalinas.jfern@gmail.com>
2018-05-20 23:02:02 -0400

    Reviewed the Collapse and Exapand option. Added Front-end support and image upload support.

--- inc/widgets/class-widget-features.php
+++ inc/widgets/class-widget-features.php
@@ -13,10 +13,12 @@
 	 public function __construct() {
 		 	$widget_details = array(
 		 		'classname' => 'DoroPM_Features',
-		 		'description' => 'Creates a featured item consisting of a title, image, description and link.'
+		 		'description' => 'Creates a featured items consisting of a title, image and description.'
 		 );
- 	 		parent::__construct( 'DoroPM_Features', 'Featured Item Widget', $widget_details );
+ 	 		parent::__construct( 'DoroPM_Features', 'Features Widget', $widget_details );
+			add_action( 'admin_enqueue_scripts', array( $this, 'features_images' ) );
 }
+
 /**
  * Outputs the content of the widget
  *
@@ -25,6 +27,58 @@
  */
 public function widget( $args, $instance ) {
 	// outputs the content of the widget
+			echo $args['before_widget'];
+			if ( ! empty( $instance['main_title'] & $instance['main_description']) ) {
+					echo $args['before_title'] . apply_filters( 'widget_title', $instance['main_title'] ). $args['after_title'];
+					echo $args['before_description'] . apply_filters( 'wp_widget_description', $instance['main_description'] ). $args['after_description'];
+		}
+			// Rest of the widget content
+			?>
+			<div class="row">
+					<div class='feature-area-1 col-md-4 col-xs-12'>
+						<div class='feature-1-title'>
+							<h5><?php echo wpautop( esc_html( $instance['title_1'] ) ) ?></h5>
+						</div>
+						<div class='feature-images' id='feature-1-image'>
+							<p>
+									<img src='<?php echo $instance['image_1'] ?>'>
+							</p>
+						</div>
+						<div class='feature-1-description'>
+							<?php echo wpautop( esc_html( $instance['description_1'] ) ) ?>
+						</div>
+					</div>
+					<div class='feature-area-2 col-md-4 col-xs-12'>
+						<div class='feature-2-title'>
+							<h5><?php echo wpautop( esc_html( $instance['title_2'] ) ) ?>	</h5>
+						</div>
+						<div class='feature-images' id='feature-2-image'>
+							<p>
+									<img src='<?php echo $instance['image_2'] ?>'>
+							</p>
+						</div>
+						<div class='feature-2-description'>
+							<?php echo wpautop( esc_html( $instance['description_2'] ) ) ?>
+						</div>
+					</div>
+					<div class='feature-area-3 col-md-4 col-xs-12'>
+						<div class='feature-3-title'>
+								<h5><?php echo wpautop( esc_html( $instance['title_3'] ) ) ?>	</h5>
+						</div>
+						<div class='feature-images' id='feature-3-image'>
+							<p>
+									<img src='<?php echo $instance['image_3'] ?>'>
+							</p>
+						</div>
+						<div class='feature-3-description'>
+							<?php echo wpautop( esc_html( $instance['description_3'] ) ) ?>
+						</div>
+					</div>
+			</div>
+
+			<?php
+			echo $args['after_widget'];
+
 }
 
 /**
@@ -63,24 +117,22 @@ public function form( $instance ) {
 					}
 
 					$description_1 = '';
-					if( !empty( $instance['description1'] ) ) {
-							$description_1 = $instance['description1'];
+					if( !empty( $instance['description_1'] ) ) {
+							$description_1 = $instance['description_1'];
 					}
 
-					$link_url_1 = '';
-					if( !empty( $instance['link_url_1'] ) ) {
-							$link_url_1 = $instance['link_url_1'];
-					}
-
-					$link_title_1 = '';
-					if( !empty( $instance['link_title_1'] ) ) {
-							$link_title_1 = $instance['link_title_1'];
+					$image_1 = '';
+					if(isset($instance['image_1']))
+					{
+					    $image_1 = $instance['image_1'];
 					}
 
 					?>
 					<fieldset class="field_set">
 							<legend><strong>Feature 1</strong></legend>
-								<p class='expand'>+</p>
+							<div class="expand-area">
+ 							 	<p class='expand'>+</p>
+ 							</div>
 								<div class="feature_content">
 
 									<p>
@@ -94,14 +146,10 @@ public function form( $instance ) {
 									</p>
 
 									<p>
-											<label for="<?php echo $this->get_field_name( 'link_url_1' ); ?>"><?php _e( 'Link URL:' ); ?></label>
-											<input class="widefat" id="<?php echo $this->get_field_id( 'link_url_1' ); ?>" name="<?php echo $this->get_field_name( 'link_url_1' ); ?>" type="text" value="<?php echo esc_attr( $link_url_1 ); ?>" />
-									</p>
-
-									<p>
-											<label for="<?php echo $this->get_field_name( 'link_title_1' ); ?>"><?php _e( 'Link Title:' ); ?></label>
-											<input class="widefat" id="<?php echo $this->get_field_id( 'link_title_1' ); ?>" name="<?php echo $this->get_field_name( 'link_title_1' ); ?>" type="text" value="<?php echo esc_attr( $link_title_1 ); ?>" />
-									</p>
+									    <label for="<?php echo $this->get_field_name( 'image_1' ); ?>"><?php _e( 'Image:' ); ?></label>
+									    <input name="<?php echo $this->get_field_name( 'image_1' ); ?>" id="<?php echo $this->get_field_id( 'image_1' ); ?>" class="widefat" type="text" size="36"  value="<?php echo esc_url( $image_1 ); ?>" />
+									    <input class="upload_image_button" type="button" value="Upload Image" />
+		             </p>
 								</div>
 							</fieldset>
 					<?php
@@ -115,24 +163,22 @@ public function form( $instance ) {
 							$description_2 = $instance['description_2'];
 					}
 
-					$link_url_2 = '';
-					if( !empty( $instance['link_url_2'] ) ) {
-							$link_url_2 = $instance['link_url_2'];
-					}
-
-					$link_title_2 = '';
-					if( !empty( $instance['link_title_2'] ) ) {
-							$link_title_2 = $instance['link_title_2'];
+					$image_2 = '';
+					if(isset($instance['image_2']))
+					{
+					    $image_2 = $instance['image_2'];
 					}
 
 					?>
 					<fieldset class="field_set">
 						<legend><strong>Feature 2</strong></legend>
-						<p class='expand'>+</p>
+						<div class="expand-area">
+							 <p class='expand'>+</p>
+						</div>
 						<div class="feature_content">
 								<p>
 										<label for="<?php echo $this->get_field_name( 'title_2' ); ?>"><?php _e( 'Title:' ); ?></label>
-										<input class="widefat" id="<?php echo $this->get_field_id( 'title_2' ); ?>" name="<?php echo $this->get_field_name( 'title_2' ); ?>" type="text" value="<?php echo esc_attr( $title_2_2 ); ?>" />
+										<input class="widefat" id="<?php echo $this->get_field_id( 'title_2' ); ?>" name="<?php echo $this->get_field_name( 'title_2' ); ?>" type="text" value="<?php echo esc_attr( $title_2 ); ?>" />
 								</p>
 
 								<p>
@@ -141,20 +187,16 @@ public function form( $instance ) {
 								</p>
 
 								<p>
-										<label for="<?php echo $this->get_field_name( 'link_url_2' ); ?>"><?php _e( 'Link URL:' ); ?></label>
-										<input class="widefat" id="<?php echo $this->get_field_id( 'link_url_2' ); ?>" name="<?php echo $this->get_field_name( 'link_url_2' ); ?>" type="text" value="<?php echo esc_attr( $link_url_2 ); ?>" />
-								</p>
-
-								<p>
-										<label for="<?php echo $this->get_field_name( 'link_title_2' ); ?>"><?php _e( 'Link Title:' ); ?></label>
-										<input class="widefat" id="<?php echo $this->get_field_id( 'link_title_2' ); ?>" name="<?php echo $this->get_field_name( 'link_title_2' ); ?>" type="text" value="<?php echo esc_attr( $link_title_2 ); ?>" />
-								</p>
+								    <label for="<?php echo $this->get_field_name( 'image_2' ); ?>"><?php _e( 'Image:' ); ?></label>
+								    <input name="<?php echo $this->get_field_name( 'image_2' ); ?>" id="<?php echo $this->get_field_id( 'image_2' ); ?>" class="widefat" type="text" size="36"  value="<?php echo esc_url( $image_2 ); ?>" />
+								    <input class="upload_image_button" type="button" value="Upload Image" />
+	             </p>
 							</div>
 						</fieldset>
 					<?php
 					$title_3 = '';
 					if( !empty( $instance['title_3'] ) ) {
-							$title = $instance['title_3'];
+							$title_3 = $instance['title_3'];
 					}
 
 					$description_3 = '';
@@ -162,20 +204,18 @@ public function form( $instance ) {
 							$description_3 = $instance['description_3'];
 					}
 
-					$link_url_3 = '';
-					if( !empty( $instance['link_url_3'] ) ) {
-							$link_url_3 = $instance['link_url_3'];
-					}
 
-					$link_title_3 = '';
-					if( !empty( $instance['link_title_3'] ) ) {
-							$link_title_3 = $instance['link_title_3'];
+					$image_3 = '';
+					if(isset($instance['image_3']))
+					{
+					    $image_3 = $instance['image_3'];
 					}
-
 					?>
 					<fieldset class="field_set">
 						<legend><strong>Feature 3</strong></legend>
-						  <p class='expand'>+</p>
+						 <div class="expand-area">
+							 	<p class='expand'>+</p>
+							</div>
 						  <div class="feature_content">
 
 							<p>
@@ -189,14 +229,10 @@ public function form( $instance ) {
 							</p>
 
 							<p>
-									<label for="<?php echo $this->get_field_name( 'link_url_3' ); ?>"><?php _e( 'Link URL:' ); ?></label>
-									<input class="widefat" id="<?php echo $this->get_field_id( 'link_url_3' ); ?>" name="<?php echo $this->get_field_name( 'link_url_3' ); ?>" type="text" value="<?php echo esc_attr( $link_url_3 ); ?>" />
-							</p>
-
-							<p>
-									<label for="<?php echo $this->get_field_name( 'link_title_3' ); ?>"><?php _e( 'Link Title:' ); ?></label>
-									<input class="widefat" id="<?php echo $this->get_field_id( 'link_title_3' ); ?>" name="<?php echo $this->get_field_name( 'link_title_3' ); ?>" type="text" value="<?php echo esc_attr( $link_title_3 ); ?>" />
-							</p>
+							    <label for="<?php echo $this->get_field_name( 'image_3' ); ?>"><?php _e( 'Image:' ); ?></label>
+							    <input name="<?php echo $this->get_field_name( 'image_3' ); ?>" id="<?php echo $this->get_field_id( 'image_3' ); ?>" class="widefat" type="text" size="36"  value="<?php echo esc_url( $image_3 ); ?>" />
+							    <input class="upload_image_button" type="button" value="Upload Image" />
+             </p>
 						</div>
 					</fieldset>
 
@@ -209,19 +245,19 @@ public function form( $instance ) {
 			  $( ".feature_content" ).hide();
 			});
 
-			$(".field_set").click(function(){
-				if($(this).children(".feature_content" ).is(":visible")){
-					$(this).children(".feature_content" ).slideUp('slow');
-					$(this).children(".expand").text('+');
-				}else {
-					$(this).children(".feature_content" ).slideDown('slow');
+			$(".expand-area").click(function(){
+				if($(this).next().is(":hidden")){
+					$(this).next().slideDown('slow');
 					$(this).children(".expand").text('-');
+				}else {
+					$(this).next().slideUp('slow');
+					$(this).children(".expand").text('+');
 				}
 			});
 
 			</script>
 
-			<style>.expand{font-size: 2em;line-height: 0;margin: 10px;margin-bottom:20px;float: right;}.field_set{border: 1px solid #ddd;margin-top: 10px;}.feature_content{padding: 10px;}</style>
+			<style>p.expand { margin: 0; padding-right: 20px; font-weight: 500; font-size: 2em; line-height: 20px; margin-bottom: 10px; text-align: right; }.field_set{border: 1px solid #ddd;margin-top: 10px;}.feature_content{padding: 10px;}.feature_content{padding: 10px;padding-top: 0; margin-top: 0px;}</style>
 <?php
 }
 
@@ -235,6 +271,14 @@ public function form( $instance ) {
  */
 public function update( $new_instance, $old_instance ) {
 	// processes widget options to be saved
+	return $new_instance;
 }
+public function features_images()
+{
+    wp_enqueue_script('media-upload');
+    wp_enqueue_script('thickbox');
+    wp_enqueue_script('features-media-upload', get_template_directory_uri()  . '/js/features-media-uploads.js', array( 'jquery' )) ;
+    wp_enqueue_style('thickbox');
 
+}
 }
